7d7756067672da98073d7823e2d7b88775484081
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-05-29 12:23:09 +0200

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct Character {
+	char* characterClass;
+	char* Gender;
+}Character;
+
+Character* createCharacter();
+void saveCharacterToFile(Character* character);
+
+#endif
