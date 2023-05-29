8d6d38daace2de52c8031a622ef41e5dc7842e74
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-05-29 12:28:50 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Header.h"
+
+int main() {
+	Character* character = createCharacter();
+
+	printf("Character class: %s\n",character->characterClass);
+	printf("Character gender: %s\n", character->Gender);
+
+	saveCharacterToFile(character);
+	free(character);
+
+	return 0;
+}
