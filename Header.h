ad5370a5499c87e6883e74b415d6d98b99d9b005
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-01 15:48:01 +0200

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,16 +1,39 @@
 #pragma once
 #ifndef HEADER_H
 #define HEADER_H
+//.H za raščlanjivanje i izvršavanje
 
-//3
+extern bool parseAndExecute(char* input);
 
-typedef struct Character {
-	char* characterClass;
-	char* Gender;
-}Character;
+//.H za lokacija
 
-Character* createCharacter();
-void saveCharacterToFile(Character* character);
-void Location(char** location);
+extern void executeLook(const char* noun);
+extern void executeGo(const char* noun);
+
+//.H za objekt
+
+typedef struct object {
+	const char* description;
+	const char* tag;
+	const char* location;
+}OBJECT;
+
+extern OBJECT objs[];
+
+#define field (objs+0)
+#define cave (objs+1)
+#define gold (objs+2)
+#define bronze (objs+3)
+#define player (objs+4)
+
+#define endOfObjs(objs+5)
+
+//.H za funkcije imenica
+
+extern OBJECT* getVisible(const char* obj, const char* noun);
+
+//.H za pomoćnu funkciju
+
+extern int listObjectsAtLocation(OBJECT* location);
 
 #endif
