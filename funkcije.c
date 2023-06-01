bd7b85d8af60f1cb93cf9d62d586c88466f1f599
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-01 16:55:39 +0200

    Update funkcije.c

--- funkcije.c
+++ funkcije.c
@@ -28,41 +28,6 @@ bool parseAndExecute(char* input) {
 
 //Funkcije za lokaciju, struktura, korištenje pokazivača, 
 
-struct location {
-	const char* description;
-	const char* tag;
-}locs[] = {
-	{"", ""}, {"", ""}
-};
-
-#define numberOfLocations (sizeof locs / sizeof *locs)
-
-static unsigned locationOfPlayer = 0;
-
-void executeLook(const char* noun) {
-	if (noun != NULL && strcmp(noun, "around") == 0) {
-		printf("You are in %s.\n", player->location->description);
-		listObjectsAtLocation(player->location);
-	}
-	else {
-		printf("I don't understand what you want to see.\n");
-	}
-}
-
-void executeGo(const char* noun) {
-	OBJECT* obj = getVisible("Where you want to go", noun);
-	if (obj == NULL) {
-		//getVisible je ovo riješio
-	}
-	else if (obj->location == NULL && obj != player->location) {
-		printf("Ok.\n");
-		player->location = obj;
-		executeLook("around");
-	}
-	else {
-		printf("You can't get much closer than this.\n");
-	}
-}
 
 //Funkcije za objekte
 
