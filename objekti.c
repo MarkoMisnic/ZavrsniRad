6c9cae260ccc096fb1f192a9baacc91780ddbb93
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-05 22:44:51 +0200

    Create objekti.c

---
+++ objekti.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "objekti.h"
+
+
+OBJECT objs[] = {
+   {"an open field"  , "field"   , NULL , NULL  },
+   {"a cramped cave"  , "cave"    , NULL , NULL  },
+   {"a silver coin"  , "silver"  , field, NULL  },
+   {"a gold bar"    , "gold"    , cave , NULL  },
+   {"a strong guard"  , "guard"   , field, NULL  },
+   {"yourself"       , "yourself", field, NULL  },
+   {"a cave entrance", "entrance", field, cave  },
+   {"an exit"        , "exit"    , cave , field }
+};
