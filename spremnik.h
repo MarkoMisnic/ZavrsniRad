69ef4b0ff2f2abaf56ee75f684daf899fd3ee8f3
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-05 22:45:31 +0200

    Create spremnik.h

---
+++ spremnik.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef SPREMNIK_H
+#define SPREMNIK_H
+
+extern void executeGet(const char* noun);
+extern void executeDrop(const char* noun);
+extern void executeAsk(const char* noun);
+extern void executeGive(const char* noun);
+extern void executeInventory(void);
+
+
+#endif
