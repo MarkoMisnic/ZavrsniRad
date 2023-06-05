65de9e0057eb48311af7d83b247918466d3b0276
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-05 22:42:45 +0200

    Update lokacije.c

--- lokacije.c
+++ lokacije.c
@@ -1,43 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
-#include "Header.h"
+#include "objekti.h"
 
-//Funkcije za lokaciju, struktura, korištenje pokazivača, 
-
-struct location{
-	const char* description;
-	const char* tag;
-} locs[] = {
-	{"", ""}, {"", ""}
+struct location {
+    const char* description;
+    const char* tag;
+}
+locs[] = {
+   {"an open field", "field"},
+   {"a little cave", "cave"}
 };
-
-#define numberOfLocations (sizeof locs / sizeof *locs)
-
+#define numberOfLocations   (sizeof locs / sizeof *locs)
 static unsigned locationOfPlayer = 0;
 
-void executeLook(const char* noun) {
-	if (noun != NULL && strcmp(noun, "around") == 0) {
-		printf("You are in %s.\n", player->location);
-		listObjectsAtLocation(player->location);
-	}
-	else {
-		printf("I don't understand what you want to see.\n");
-	}
-}
 
-void executeGo(const char* noun) {
-	OBJECT* obj = getVisible("Where you want to go", noun);
-	if (obj == NULL) {
-		//getVisible je ovo riješio
-	}
-	else if (obj->location == NULL && obj != player->location) {
-		printf("Ok.\n");
-		player->location = obj;
-		executeLook("around");
-	}
-	else {
-		printf("You can't get much closer than this.\n");
-	}
+void executeLook(const char* noun)
+{
+    if (noun != NULL && strcmp(noun, "around") == 0)
+    {
+        printf("You are in %s.\n", player->location->description);
+        listObjectsAtLocation(player->location);
+    }
+    else
+    {
+        printf("I don't understand what you want to see.\n");
+    }
+}
+void executeGo(const char* noun)
+{
+    OBJECT* obj = getVisible("where you want to go", noun);
+    if (obj == NULL)
+    {
+        // getVisible je riješio ovo 
+    }
+    else if (getPassage(player->location, obj) != NULL)
+    {
+        printf("OK.\n");
+        player->location = obj;
+        executeLook("around");
+    }
+    else if (obj->location != player->location)
+    {
+        printf("You don't see any %s here.\n", noun);
+    }
+    else if (obj->destination != NULL)
+    {
+        printf("OK.\n");
+        player->location = obj->destination;
+        executeLook("around");
+    }
+    else
+    {
+        printf("You can't get much closer than this.\n");
+    }
 }
