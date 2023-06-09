d74313f5b7e03d0773ef57c8a4b049eb9df9449e
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-09 02:18:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include "character.h"
+#include <stdlib.h>
+#include "charLoc.h"
+#include "quest.h"
 
 int main() {
     Character myCharacter = createCharacter();
     saveCharacterToFile(myCharacter);
+
+    Direction directions[] = { NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST };
+    int numDirections = sizeof(directions) / sizeof(directions[0]);
+
+
+    Direction chosenDirection = chooseDirection();
+    saveLocationToFile(chosenDirection);
+
+    if (chosenDirection == NORTH) {
+        printf("You have reached a desolate house in the North.\n");
+        activateQuest(myCharacter);
+    }
 
     return 0;
 }
