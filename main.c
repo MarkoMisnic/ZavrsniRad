b6e89c52482661b5f90a04e43b0e4c8d46344a63
MarkoMisnic <134614997+MarkoMisnic@users.noreply.github.com>
2023-06-10 02:00:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "charLoc.h"
 #include "quest.h"
+#include "combat.h"
+
 
 int main() {
     Character myCharacter = createCharacter();
@@ -18,6 +20,19 @@ int main() {
     if (chosenDirection == NORTH) {
         printf("You have reached a desolate house in the North.\n");
         activateQuest(myCharacter);
+
+        /*Combatant* player = createCombatant(myCharacter.name, 100, 20, 10);
+        Combatant* enemy = createCombatant("Goblin", 80, 15, 5);
+
+        if (player != NULL && enemy != NULL) {
+            performCombat(player, enemy);
+        }
+
+        destroyCombatant(&player);
+        destroyCombatant(&enemy);*/
+    }
+    else {
+        checkLocation(chosenDirection);
     }
 
     return 0;
